9cfa4e25ec22b1dfe4b890c15f12e7eea2e2af34
Harish kumar <hdharish0101@gmail.com>
2024-08-27 12:21:16 +0530

    removed darkmode from signup and signin

--- frontend/src/components/InputBox.tsx
+++ frontend/src/components/InputBox.tsx
@@ -9,7 +9,7 @@ interface InputBoxType {
 export const InputBox = ({label,placeholder,onChange,type}:InputBoxType) => {
     return <div className="w-80 mx-auto mt-4">
         <div className="flex justify-start">
-            <label className="block mb-2 text-sm font-medium text-cyan-800 dark:text-white">{label}</label>
+            <label className="block mb-2 text-sm font-medium text-cyan-800">{label}</label>
         </div>
         <div>
             <input onChange={onChange} type={type || "text"} id="first_name" className="bg-cyan-50 border border-sky-200 text-cyan-900 text-sm rounded-lg focus:ring-cyan-500 focus:border-cyan-500 block w-80 p-2.5 " placeholder={placeholder}/>
